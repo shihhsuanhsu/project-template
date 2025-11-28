9e1c074c079f809a246ac68077fda5ea03e08f40
Shih-Hsuan (Daniel) Hsu <shhsu@cs.washington.edu>
2025-11-28 17:42:57 -0600

    update colors

--- project-template.md
+++ project-template.md
@@ -1,11 +1,15 @@
 # Project Template
 
-Version: 0.2.12
+Version: 0.2.13
 
 **NOTE:** Please do not modify this file.
 Shih-Hsuan uses this file to track the project template version.
 
 ## Update log
+
+### 0.2.13
+
+- Update colors 
 
 ### 0.2.12
 
--- tasks/common/code/plot_settings.py
+++ tasks/common/code/plot_settings.py
@@ -4,6 +4,7 @@
 
 import itertools
 import pikepdf
+from pyparsing import Iterator
 import plotly.io as pio
 import plotly.graph_objects as go
 import pandas as pd
@@ -21,6 +22,8 @@ DEFAULT_HEIGHT2 = 600
 """default height of the figure"""
 IMAGE_FORMATS = ["pdf", "html"]
 """image formats"""
+# set the default scale of the image
+SCALE = 1.5
 
 INDICATOR_COLOR_SCALE = [
     (0, "#6a9f58"),
@@ -37,15 +40,18 @@ LINE_OPACITY = 0.8
 CI_OPACITY = 0.2
 """opacity for the confidence interval"""
 
-# modification of the default template
-pio.templates["my_mod"] = go.layout.Template(
-    layout={
+CUSTOM_TEMPLATE = {
+    "layout": {
         "font": {
-            "size": 16,
-            "family": "Roboto",
+            "size": 18,
+            "family": "Arial",
         },
         "legend": {
-            "font": {"size": 14, "color": "black"},
+            "font": {
+                "size": 16,
+                "color": "black",
+                "family": "Arial",
+            },
             "traceorder": "normal",
         },
         "margin": {"l": 20, "r": 20, "t": 35, "b": 20},
@@ -54,104 +60,78 @@ pio.templates["my_mod"] = go.layout.Template(
         "title": {
             "font": {
                 "color": "black",
+                "family": "Arial",
             },
         },
     },
-    data={
-        "scatter": [go.Scatter(line={"width": 2}, marker={"size": 6})],
-        "scattergl": [go.Scattergl(line={"width": 4}, marker={"size": 8})],
+    "data": {
+        "scatter": [
+            go.Scatter(
+                line={"width": 4}, marker={"size": 6}, opacity=LINE_OPACITY
+            )
+        ],
+        "scattergl": [
+            go.Scattergl(
+                line={"width": 6}, marker={"size": 8}, opacity=LINE_OPACITY
+            )
+        ],
     },
-)
+}
+"""custom template for the plots (16 by 9 aspect ratio)"""
+CUSTOM_TEMPLATE_32 = CUSTOM_TEMPLATE.copy()
+"""custom template for the plots (3 by 2 aspect ratio)"""
+CUSTOM_TEMPLATE_32["layout"]["font"]["size"] += 2
+CUSTOM_TEMPLATE_32["layout"]["legend"]["font"]["size"] += 2
+CUSTOM_TEMPLATE_32["data"]["scatter"][0]["line"]["width"] += 1
+CUSTOM_TEMPLATE_32["data"]["scattergl"][0]["line"]["width"] += 1
+CUSTOM_TEMPLATE_32["data"]["scatter"][0]["marker"]["size"] += 1
+CUSTOM_TEMPLATE_32["data"]["scattergl"][0]["marker"]["size"] += 1
+
+
+# modification of the default template
+pio.templates["my_mod"] = go.layout.Template(CUSTOM_TEMPLATE)
+pio.templates["my_mod_32"] = go.layout.Template(CUSTOM_TEMPLATE_32)
 
 # set the default template
 pio.templates.default = "simple_white+my_mod"
 
-# set the default scale of the image
-SCALE = 1.5
-
-# colors for line and CI
 COLORS = [
-    ("rgba(31, 119, 180, 1)", "rgba(31, 119, 180, 0.2)"),  # Blue
-    ("rgba(255, 127, 14, 1)", "rgba(255, 127, 14, 0.2)"),  # Orange
-    ("rgba(214, 39, 40, 1)", "rgba(214, 39, 40, 0.2)"),  # Red
-    ("rgba(44, 160, 44, 1)", "rgba(44, 160, 44, 0.2)"),  # Green
-    ("rgba(148, 103, 189, 1)", "rgba(148, 103, 189, 0.2)"),  # Purple
-    ("rgba(140, 86, 75, 1)", "rgba(140, 86, 75, 0.2)"),  # Brown
-    ("rgba(227, 119, 194, 1)", "rgba(227, 119, 194, 0.2)"),  # Pink
-    # ('rgba(127, 127, 127, 1)', 'rgba(127, 127, 127, 0.2)'),  # Gray
-    ("rgba(188, 189, 34, 1)", "rgba(188, 189, 34, 0.2)"),  # Olive
-    ("rgba(23, 190, 207, 1)", "rgba(23, 190, 207, 0.2)"),  # Cyan
-    # ("rgba(255, 187, 120, 1)", "rgba(255, 187, 120, 0.2)"),  # Light Orange
-    # ("rgba(152, 223, 138, 1)", "rgba(152, 223, 138, 0.2)"),  # Light Green
-    # ("rgba(255, 152, 150, 1)", "rgba(255, 152, 150, 0.2)"),  # Light Red
-    # ("rgba(197, 176, 213, 1)", "rgba(197, 176, 213, 0.2)"),  # Light Purple
-    # ("rgba(196, 156, 148, 1)", "rgba(196, 156, 148, 0.2)"),  # Light Brown
-    # ("rgba(247, 182, 210, 1)", "rgba(247, 182, 210, 0.2)"),  # Light Pink
-    # # ("rgba(199, 199, 199, 1)", "rgba(199, 199, 199, 0.2)"),  # Light Gray
-    # ("rgba(219, 219, 141, 1)", "rgba(219, 219, 141, 0.2)"),  # Light Olive
-    # ("rgba(158, 218, 229, 1)", "rgba(158, 218, 229, 0.2)"),  # Light Cyan
-    # ("rgba(255, 205, 86, 1)", "rgba(255, 205, 86, 0.2)"),  # Yellow
-    # ("rgba(75, 192, 192, 1)", "rgba(75, 192, 192, 0.2)"),  # Teal
-    # ("rgba(255, 99, 132, 1)", "rgba(255, 99, 132, 0.2)"),  # Coral
-    # ("rgba(153, 102, 255, 1)", "rgba(153, 102, 255, 0.2)"),  # Lavender
-    # ("rgba(255, 159, 64, 1)", "rgba(255, 159, 64, 0.2)"),  # Orange-Yellow
-    # ("rgba(54, 162, 235, 1)", "rgba(54, 162, 235, 0.2)"),  # Sky Blue
-    # # ("rgba(201, 203, 207, 1)", "rgba(201, 203, 207, 0.2)"),  # Light Blue Gray
-    # ("rgba(255, 204, 204, 1)", "rgba(255, 204, 204, 0.2)"),  # Light Pink-Red
-    # ("rgba(204, 235, 197, 1)", "rgba(204, 235, 197, 0.2)"),  # Light Mint
-    # ("rgba(222, 203, 228, 1)", "rgba(222, 203, 228, 0.2)"),  # Light Lavender
-    # ("rgba(255, 255, 179, 1)", "rgba(255, 255, 179, 0.2)"),  # Light Yellow
-    # ("rgba(128, 222, 234, 1)", "rgba(128, 222, 234, 0.2)"),  # Light Teal
-    # ("rgba(255, 153, 204, 1)", "rgba(255, 153, 204, 0.2)"),  # Light Coral
-    # (
-    #     "rgba(204, 204, 255, 1)",
-    #     "rgba(204, 204, 255, 0.2)",
-    # ),  # Light Lavender-Blue
-    # (
-    #     "rgba(255, 204, 153, 1)",
-    #     "rgba(255, 204, 153, 0.2)",
-    # ),  # Light Orange-Yellow
-    # ("rgba(153, 204, 255, 1)", "rgba(153, 204, 255, 0.2)"),  # Light Sky Blue
-    # # ("rgba(229, 229, 229, 1)", "rgba(229, 229, 229, 0.2)"),  # Light Gray-White
-    # ("rgba(255, 229, 229, 1)", "rgba(255, 229, 229, 0.2)"),  # Light Pink-White
-    # ("rgba(229, 255, 229, 1)", "rgba(229, 255, 229, 0.2)"),  # Light Mint-White
-    # (
-    #     "rgba(242, 229, 255, 1)",
-    #     "rgba(242, 229, 255, 0.2)",
-    # ),  # Light Lavender-White
-    # (
-    #     "rgba(255, 255, 229, 1)",
-    #     "rgba(255, 255, 229, 0.2)",
-    # ),  # Light Yellow-White
-    # ("rgba(204, 255, 255, 1)", "rgba(204, 255, 255, 0.2)"),  # Light Cyan-White
-    # ("rgba(255, 229, 242, 1)", "rgba(255, 229, 242, 0.2)"),  # Light Coral-White
-    # (
-    #     "rgba(229, 229, 255, 1)",
-    #     "rgba(229, 229, 255, 0.2)",
-    # ),  # Light Lavender-Blue-White
-    # (
-    #     "rgba(255, 242, 229, 1)",
-    #     "rgba(255, 242, 229, 0.2)",
-    # ),  # Light Orange-Yellow-White
-    # (
-    #     "rgba(229, 242, 255, 1)",
-    #     "rgba(229, 242, 255, 0.2)",
-    # ),  # Light Sky Blue-White
+    (1, 115, 178),  # Blue
+    (222, 143, 5),  # Orange
+    (204, 120, 188),  # Magenta
+    (2, 158, 115),  # Cyan
+    (236, 225, 51),  # Yellow
+    (213, 94, 0),  # Vermillion
+    (148, 148, 148),  # Gray
 ]
+"""List of colors in RGB format."""
+
+MARKERS = [0, 2, 4, 1, 3, 17, 18, 22, 23, 24]
 
 
-def color_iter():
+def color_iter() -> Iterator[tuple[str, str]]:
     """
     Return an iterator of colors.
+    Returns:
+        Iterator[tuple[str, str]]: An iterator of colors in rgba format for:
+            - first element: main color
+            - second element: confidence intervals
     """
-    return itertools.cycle(COLORS)
+    while True:  # cycle through colors indefinitely
+        for color in COLORS:
+            r, g, b = color
+            # yield main color and confidence interval color
+            yield (
+                f"rgba({r}, {g}, {b}, 1.0)",
+                f"rgba({r}, {g}, {b}, {CI_OPACITY})",
+            )
 
 
-def marker_iter():
+def marker_iter() -> Iterator[int]:
     """
     Return an iterator of marker symbols.
     """
-    return itertools.cycle([0, 2, 4, 1, 3, 17, 18, 22, 23, 24])
+    return itertools.cycle(MARKERS)
 
 
 def remove_pdf_metadata(filepath: str) -> None:
@@ -215,6 +195,7 @@ def save_figure(
                 engine="kaleido",  # Explicitly specify engine for consistency
             )
             if two_aspect_ratio:
+                fig.update_layout(template="simple_white+my_mod_32")
                 fig.write_image(
                     f"../output/{name}_2.{format}",
                     width=width2,
@@ -257,3 +238,27 @@ def construct_shapes(
         for _, group in groups
         if not group.empty
     ]
+
+
+def move_legend_to_bottom(
+    fig: go.Figure,
+    y_position_padding: float = 0.0,
+    additional_settings: dict = {},
+) -> None:
+    """
+    Move the legend to the bottom of the figure with specified y position.
+    Args:
+        fig (go.Figure): The figure to modify.
+        y_position_padding (float): The padding to adjust the y position of the legend. Default is 0.0.
+    """
+    fig.update_layout(
+        legend={
+            "orientation": "h",
+            "yanchor": "bottom",
+            "y": -0.3 + y_position_padding,
+            "xanchor": "center",
+            "x": 0.5,
+            "traceorder": "normal",
+            **additional_settings,
+        }
+    )
